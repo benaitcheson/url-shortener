c9ce6bd25d78c3b0c65cd6e494f527c4cb830fad
Ben Aitcheson <benaitcheson@hotmail.com>
2023-07-09 11:34:46 +1000

    ADD: import all the npm stuff

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {  BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import initializeApp from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA9sejPD9tr8rgvgluxgv_cS_P2qEbxyTg",
+  authDomain: "url-shortener-de4fc.firebaseapp.com",
+  projectId: "url-shortener-de4fc",
+  storageBucket: "url-shortener-de4fc.appspot.com",
+  messagingSenderId: "925197965707",
+  appId: "1:925197965707:web:533e61a5ef4b8eab59378e",
+  measurementId: "G-1F8TTDDN5S"
+};
+
+initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
